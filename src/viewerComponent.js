570a13e8bd421468a16c6734a12e2066797b7f86
Z3 Development <z3-dev@gfnews.net>
2020-10-09 16:45:21 +0900

    feat(viewerComponent): integrated VUE store into the viewerComponent, allowing design solids to be set at anytime

--- src/viewerComponent.js
+++ src/viewerComponent.js
@@ -8,7 +8,12 @@ let numberOfInstances = 0
 const renderRate = 10 // number of renders per second
 const zoomRate = 10 // number of zooms per second
 
-
+/**
+ * Set up the JSCAD renderer.
+ * @param {DOM Element} containerElement - HTML element of which to add the renderer (canvas)
+ * @param {Data} data - viewer data (value of options)
+ * @return new JSCAD renderer (already running)
+ */
 const setupRenderer = (containerElement, data) => {
   const width = containerElement.clientWidth
   const height = containerElement.clientHeight
@@ -22,8 +27,7 @@ const setupRenderer = (containerElement, data) => {
       isOrbiting: false,
       lastClick: 0, // ms
       lastZoom: 0
-    },
-    entities: [] // converted solids
+    }
   }
 
   // prepare the camera
@@ -59,26 +63,19 @@ const setupRenderer = (containerElement, data) => {
       show: data.axisOptions.show
     }
   }
-  const renderOptions = {
+  data.state.content = {
+    // define the visual content
     camera: data.state.camera,
     drawCommands: {
       drawGrid: drawCommands.drawGrid,
       drawAxis: drawCommands.drawAxis,
       drawMesh: drawCommands.drawMesh
     },
-    // define the visual content
     entities: [
       gridOptions,
       axisOptions
       //...solids
     ]
-  }
-  if (solids) {
-    data.state.entities = entitiesFromSolids({}, solids)
-    renderOptions.entities.push(...data.state.entities)
-
-    const updated = orbitControls.zoomToFit({ controls: data.state.controls, camera: data.state.camera, entities: data.state.entities })
-    data.state.controls = { ...data.state.controls, ...updated.controls }
   }
 
   // the heart of rendering, as themes, controls, etc change
@@ -93,7 +90,7 @@ const setupRenderer = (containerElement, data) => {
       data.state.camera.position = updates.camera.position
       perspectiveCamera.update(data.state.camera)
 
-      renderer(renderOptions)
+      renderer(data.state.content)
     }
     window.requestAnimationFrame(updateAndRender)
   }
@@ -102,10 +99,24 @@ const setupRenderer = (containerElement, data) => {
   return renderer
 }
 
+/**
+ * Update the state of the viewer, replacing the solids.
+ * @param {State) state - current state of the viewer
+ * @param {Array} solids - an array of JSCAD geometries (geom2, geom3, path2)
+ */
+const updateSolids = (state, solids) => {
+  state.content.entities = state.content.entities.filter((entity) => !entity.type) // remove old solids
+  let entities = entitiesFromSolids({}, solids)
+  state.content.entities.push(...entities)
+
+  const updated = orbitControls.zoomToFit({ controls: state.controls, camera: state.camera, entities })
+  state.controls = { ...state.controls, ...updated.controls }
+}
+
 const viewerComponent = {
   name: 'jscad-viewer',
   properties: {
-    props: [ 'solids', 'likes' ],
+    props: [ 'likes' ],
     data: function () {
       // options which can be changed
       return {
@@ -128,6 +139,19 @@ const viewerComponent = {
         }
       }
     },
+    computed: {
+      count () {
+        const state = this._data.state
+        if (state) {
+          updateSolids(state, this.$store.state.solids)
+        }
+        // update the fake DOM entry
+        return this.$store.state.count
+      },
+      solids () {
+        return this.$store.state.solids
+      }
+    },
     methods: {
       onMouseDown: function (event) {
         const state = this._data.state
@@ -147,7 +171,9 @@ console.log('d',state.mouse.buttons)
            const ms = now - state.mouse.lastClick
            if (ms < this.viewerOptions.doubleClickSpeed) {
              if (state.mouse.isOrbiting) {
-               const updated = orbitControls.zoomToFit({ controls: state.controls, camera: state.camera, entities: state.entities })
+console.log(state)
+               const entities = state.content.entities
+               const updated = orbitControls.zoomToFit({ controls: state.controls, camera: state.camera, entities })
                state.controls = { ...state.controls, ...updated.controls }
              }
            }
@@ -182,7 +208,7 @@ console.log('d',state.mouse.buttons)
         }
       }
     },
-    // Vue lifecycle additions
+    // VUE lifecycle additions
     created: function () {
       numberOfInstances++
       this.id = numberOfInstances
@@ -191,10 +217,9 @@ console.log('d',state.mouse.buttons)
       this.$el.id = `viewer${this.id}`
 
       this.renderer = setupRenderer(this.$el, this.$data)
-console.log(this.likes)
     },
-    // HTML template for the viewer (and 3D canvas)
-    template: '<div class="viewer" v-on:mousemove="onMouseMove" v-on:mousedown="onMouseDown" v-on:mouseup="onMouseUp" v-on:wheel="onScroll"></div>'
+    // VUE HTML template for the viewer (and 3D canvas)
+    template: '<div class="viewer" v-on:mousemove="onMouseMove" v-on:mousedown="onMouseDown" v-on:mouseup="onMouseUp" v-on:wheel="onScroll"><p hidden>{{ count }}</p></div>'
   }
 }
 
